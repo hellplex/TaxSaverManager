8b61b3dd7cf339a4ed3512b698f0a22478c050a4
hellplex <csuarezcorzo@gmail.com>
2016-01-16 16:28:46 +0000

    last version of DB dump

--- sql/TaxSaver01.sql
+++ sql/TaxSaver01.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 16, 2016 at 02:56 PM
+-- Generation Time: Jan 16, 2016 at 05:28 PM
 -- Server version: 5.6.26
 -- PHP Version: 5.5.28
 
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `txs_ticket_category` (
   `tcktcat_id` int(11) NOT NULL,
   `tcktcat_name` varchar(20) NOT NULL,
   `tcktcat_url` varchar(200) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `txs_ticket_category`
@@ -63,13 +63,21 @@ INSERT INTO `txs_ticket_category` (`tcktcat_id`, `tcktcat_name`, `tcktcat_url`) 
 CREATE TABLE IF NOT EXISTS `txs_ticket_type` (
   `ticket_typeId` int(11) NOT NULL,
   `ticket_name` varchar(60) NOT NULL,
-  `ticket_gross` tinyint(4) NOT NULL,
-  `ticket_net1` tinyint(4) NOT NULL,
-  `ticket_net2` tinyint(4) NOT NULL,
+  `ticket_gross` decimal(4,0) NOT NULL,
+  `ticket_net1` decimal(4,0) NOT NULL,
+  `ticket_net2` decimal(4,0) NOT NULL,
   `ticket_shortDescript` varchar(500) NOT NULL,
   `ticket_longDescript` varchar(500) NOT NULL,
   `tcktcat_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `txs_ticket_type`
+--
+
+INSERT INTO `txs_ticket_type` (`ticket_typeId`, `ticket_name`, `ticket_gross`, `ticket_net1`, `ticket_net2`, `ticket_shortDescript`, `ticket_longDescript`, `tcktcat_id`) VALUES
+(2, 'LUAS only', '91', '64', '46', 'Monthly All Zones', 'With Tax Saver Commuter Tickets from Luas it is easy for employers and employees to make a significant saving on their tax bill: no more queuing for purchasing your ticket. Benefits include: Up to 49.5% travel costs savings for staff Up to 10.75% savings for employers on PRSI No more queues for Luas Tickets Journeys are cheaper with prepaid Tickets than with cash', 1),
+(3, 'BUS only', '91', '64', '46', 'Monthly Dublin BUS', 'With Tax Saver Commuter Tickets from Luas it is easy for employers and employees to make a significant saving on their tax bill: no more queuing for purchasing your ticket. Benefits include: Up to 49.5% travel costs savings for staff Up to 10.75% savings for employers on PRSI No more queues for Luas Tickets Journeys are cheaper with prepaid Tickets than with cash', 2);
 
 -- --------------------------------------------------------
 
@@ -95,7 +103,9 @@ INSERT INTO `txs_user` (`usr_email`, `usr_password`, `usr_firstName`, `usr_lastN
 ('csuarez10@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'bb8', 'the robot', 'ufgghjgjh', 0, 0),
 ('csuarez12@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'jghjhv', 'hgjhkg', 'hgkjg', 0, 1),
 ('csuarez15@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'Cristina', 'Suarez', 't58576', 3, 0),
+('csuarez17@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'Crig', 'hghjg', 'hjgh', 0, 0),
 ('csuarez1@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'Cristina', 'Suarez Corzo', '343f', 3, 1),
+('csuarez20@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'gdfhkhjb', 'hjgkh', 'gjh', 0, 0),
 ('csuarez2@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'Cristina', 'Suarez Corzo', '343f', 2, 0),
 ('csuarez3@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'ytuyt', 'uyt', 'yut', 0, 0),
 ('csuarez4@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'tuytyh', 'ghj', 'gjh', 0, 0),
@@ -152,12 +162,12 @@ ALTER TABLE `txs_request`
 -- AUTO_INCREMENT for table `txs_ticket_category`
 --
 ALTER TABLE `txs_ticket_category`
-  MODIFY `tcktcat_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `tcktcat_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `txs_ticket_type`
 --
 ALTER TABLE `txs_ticket_type`
-  MODIFY `ticket_typeId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ticket_typeId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- Constraints for dumped tables
 --
